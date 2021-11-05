7d17494f7edd3de9db4a556bd59ff04ec4c75e1b
Sabareesh Vishwanathan <sabareesh.vishwanathan@stonybrook.edu>
2021-11-04 20:19:00 -0400

    added login route to banner

--- client/src/components/AppBanner.js
+++ client/src/components/AppBanner.js
@@ -49,6 +49,7 @@ export default function AppBanner() {
             onClose={handleMenuClose}
         >
             <MenuItem onClick={handleMenuClose}><Link to='/register/'>Create New Account</Link></MenuItem>
+            <MenuItem onClick={handleMenuClose}><Link to='/login/'>Login</Link></MenuItem>
         </Menu>
     );
     const loggedInMenu = 
